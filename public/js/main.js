5080133e835573809ba1ddf2687dcd2f91bdad35
BELEN BUSSI <bussibelen1@gmail.com>
2022-07-31 18:19:55 -0300

    prueba js

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+var boton=document.querySelector(".header__icon")
+var menu=document.querySelector(".header__navigation")
+var cont=0
+
+boton.onclick=function(){
+  if(cont%2==0){
+    menu.classList.add("header__navigation--active")
+    menu.classList.remove("header__navigation--disabled")
+  }
+  else{
+    menu.classList.remove("header__navigation--active")
+    menu.classList.add("header__navigation--disabled")
+  }
+  cont++
+}
